9abe5028a6cb886fc7e40a07b88aaf33fa0e88bc
denimubarokhilmi <hilmimubarokdeni@gmail.com>
2026-04-21 21:48:27 +0700

    feat:understand and learn what JSX is and how to create components in React

---
+++ src/components/FoundationalReact./ComponentWithClass.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Greeting extends Component {
+  render() {
+    return (
+      <>
+        <p>ini adalah component dengan class</p>
+      </>
+    );
+  }
+}
+
+export default Greeting;
