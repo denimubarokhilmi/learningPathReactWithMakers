d4f4975d71a4e280563afc4751361be1127d366b
denimubarokhilmi <hilmimubarokdeni@gmail.com>
2026-04-21 22:16:05 +0700

    feat:understanding props and state

--- src/components/FoundationalReact./TempFoundational.jsx
+++ src/components/FoundationalReact./TempFoundational.jsx
@@ -1,11 +1,13 @@
 import ComponentWithFunction from "./ComponentWithFunction";
 import Greeting from "./ComponentWithClass";
+import StateAndProps from "./State_Props";
 function TempFoundational() {
   return (
     <div>
       <h1>1. Foundational React : </h1>
       <ComponentWithFunction></ComponentWithFunction>
       <Greeting></Greeting>
+      <StateAndProps></StateAndProps>
     </div>
   );
 }
