d806e779ec8ce51aa343c39c5bec749b84cdd2b6
denimubarokhilmi <hilmimubarokdeni@gmail.com>
2026-04-21 21:43:03 +0700

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        learning React with makers
+      </h1>
+    </>
+  );
+}
+
+export default App;
